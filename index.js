bddc3bbb3d38303826ed73d8f7a02e8deee9ac9a
Your Name <your-email@example.com>
2026-01-06 15:18:21 +0530

    adding authentication

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
  const product=fetch('https://dummyjson.com/products')
     .then(res => res.json())
-    .then(product =>{ console.log(product);
+    .then(prod =>{ console.log(product);
 
-        const display=product.products.map(items=>{
+        const display=prod.products.map(items=>{
             return`
             <div class="card" ${key=items.id}>
             <img class="img" src=${items?.images}>
